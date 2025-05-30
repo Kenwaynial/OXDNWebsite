7542e315517189aedeb4a97697b29d949c289570
Kenway <josekent804@gmail.com>
2025-05-30 23:04:00 +0800

    Update supabase.js

--- config/supabase.js
+++ config/supabase.js
@@ -59,7 +59,7 @@ export const signInWithGoogle = async () => {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `${window.location.origin}/auth/callback`
+      redirectTo: `${SITE_URL}/html/auth/callback.html`
     }
   })
   return { data, error }
